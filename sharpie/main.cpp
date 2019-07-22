271a247debb7f4a5fdef59a6ed139bb11f9d9a63
Matt Button <matthew@sigswitch.com>
2019-07-22 12:21:55 +0100

    Initial Commit

---
+++ sharpie/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sharpie
+//
+//  Created by Matt Button on 22/07/2019.
+//  Copyright © 2019 Geckoboard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
